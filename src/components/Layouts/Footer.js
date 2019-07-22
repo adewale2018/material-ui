66f6f048edcfd5d5010a52d3b11a388e5666920e
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-07-22 21:26:23 +0100

    Reafactor left and right pane

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Paper, Tabs, Tab } from '@material-ui/core';
+
 
 export default props => {
   return(
-    <div>
-    <h1>FOOTER</h1>
-  </div>
+    <Paper>
+    <Tabs
+      value={0}
+      indicatorColor="primary"
+      textColor="primary"
+      centered
+    >
+      <Tab label="Item One" />
+      <Tab label="Item Two" />
+      <Tab label="Item Three" />
+    </Tabs>
+  </Paper>
   );
 }
--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import { MenuIcon } from '@material-ui/icons'; 
 
 export default props => {
   return(
-    <div>
-    <h1>HEADER</h1>
-  </div>
+    <AppBar position="static">
+        <Toolbar>
+        <Typography variant="h1" component="h2" color="inherit">
+          Exercises
+        </Typography>
+        </Toolbar>
+      </AppBar>
   );
 }
